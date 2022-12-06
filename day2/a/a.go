ae7a1388380d60d08bec1ecbda13d4eb622705e3
Felix Sperling <fsperling@salesforce.com>
2022-12-06 14:46:47 +0100

    day 2

---
+++ day2/a/a.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+import "os"
+import "strings"
+import "bufio"
+
+func check(e error) {
+  if e != nil {
+    panic(e)
+  }
+}
+
+
+func main() {
+  f, err := os.Open("../input.txt")
+  check(err)
+  var fileScanner = bufio.NewScanner(f)
+  fileScanner.Split(bufio.ScanLines)
+
+  var totalScore = 0 
+
+  for fileScanner.Scan() {
+    var score = 0
+    var match = strings.Split(fileScanner.Text(), " ")
+    
+    // Draws
+    if match[0] == "A" && match[1] == "X" ||
+      match[0] == "B" && match[1] == "Y" ||
+      match[0] == "C" && match[1] == "Z" {
+      score = score + 3
+    }
+ 
+    // Wins
+    if match[0] == "A" && match[1] == "Y" ||
+      match[0] == "B" && match[1] == "Z" ||
+      match[0] == "C" && match[1] == "X" {
+      score = score + 6
+    }
+
+    switch match[1] {
+    case "X":
+      score = score + 1
+    case "Y":
+      score = score + 2
+    case "Z":
+      score = score + 3
+    }
+    totalScore = totalScore + score
+  }
+
+  f.Close()
+  fmt.Println(totalScore)
+}
+
---
+++ day2/b/b.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+import "os"
+import "strings"
+import "bufio"
+
+func check(e error) {
+  if e != nil {
+      panic(e)
+  }
+}
+
+func main() {
+  f, err := os.Open("../input.txt")
+  check(err)
+  var fileScanner = bufio.NewScanner(f)
+  fileScanner.Split(bufio.ScanLines)
+
+  var totalScore = 0 
+
+  for fileScanner.Scan() {
+    var score = 0
+    var match = strings.Split(fileScanner.Text(), " ")
+    
+    // Rock
+    if match[0] == "A" && match[1] == "Y" ||
+      match[0] == "B" && match[1] == "X" ||
+      match[0] == "C" && match[1] == "Z" {
+      score = score + 1
+    }
+ 
+    // Paper
+    if match[0] == "A" && match[1] == "Z" ||
+      match[0] == "B" && match[1] == "Y" ||
+      match[0] == "C" && match[1] == "X" {
+      score = score + 2
+    }
+
+    // Scissor
+    if match[0] == "A" && match[1] == "X" ||
+      match[0] == "B" && match[1] == "Z" ||
+      match[0] == "C" && match[1] == "Y" {
+      score = score + 3
+    }
+    
+    switch match[1] {
+    case "X":
+      score = score + 0
+    case "Y":
+      score = score + 3
+    case "Z":
+      score = score + 6
+    }
+    totalScore = totalScore + score
+  }
+
+
+  f.Close()
+  fmt.Println(totalScore)
+}
+
